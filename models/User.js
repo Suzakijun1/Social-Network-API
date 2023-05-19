f3d8df3dadfae445522a82f490a4408227b5ad14
suzakijun1 <dyong1997@gmail.com>
2023-05-19 12:11:56 -0400

    finished user model

--- models/User.js
+++ models/User.js
@@ -1,10 +1,43 @@
 const { Schema, model } = require("mongoose");
 
-const username = new Schema({
-  first: String,
-  Unique: true,
-  required: true,
-  trimmed: true,
-});
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      Unique: true,
+      required: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      Unique: true,
+      required: true,
+      match: [/.+@.+\..+/, "Must match an email address!"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
-const courseSchema = new S();
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+const User = model("User", userSchema);
+
+module.exports = User;
