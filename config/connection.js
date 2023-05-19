0a92cad501adceea33111150cdaf19bf0ab045a5
suzakijun1 <dyong1997@gmail.com>
2023-05-18 20:18:07 -0400

    connection config

--- config/connection.js
+++ config/connection.js
@@ -2,5 +2,11 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://107.0.0.1:27017/socialmedia",
-  {}
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
+
+//Export connection
+module.exports = mongoose.connection;
