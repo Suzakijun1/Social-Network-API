fab09e87fdfeb774c55706cd180536e6f249a6c1
suzakijun1 <dyong1997@gmail.com>
2023-05-20 14:25:35 -0400

    finished getuser and getsingleuser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,25 @@
 const { User, Thought } = require("./models");
 
-const userController = {
+module.exports = {
   //Get all users
   getUsers(req, res) {
-    User.find();
+    User.find({})
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  //Get single users
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .populate("thoughts")
+      .populate("friends")
+      //excludes the __v field from the user object, which is automatically added by Mongoose to track the document version
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No User found with that ID!" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
   },
 };
