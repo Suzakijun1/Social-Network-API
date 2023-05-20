20961284e021f47f564d798c7cfcd28e6a835352
suzakijun1 <dyong1997@gmail.com>
2023-05-20 17:19:32 -0400

    more information on thoughtcontroller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -13,6 +13,33 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select("-__v")
+      .then((thought) => {
+        !thought
+          ? res.status(404).json({ message: "No Thought find with this ID!" })
+          : res.json(thought);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+  //create a thought
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then(({ _id }) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No User find with this ID!" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
 
 module.exports = thoughtController;
