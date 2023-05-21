39049577c38ba04968d172f262eaa5c0cc903959
suzakijun1 <dyong1997@gmail.com>
2023-05-21 02:42:13 -0400

    finished delete reaction inside thoughtcontroller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -89,6 +89,20 @@ const thoughtController = {
       )
       .catch((err) => res.status(500).json(err));
   },
+  //delete reaction
+  deleteReaction(req, res) {
+    Thought.findOneAndDelete(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought found with this ID!" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
 
 module.exports = thoughtController;
